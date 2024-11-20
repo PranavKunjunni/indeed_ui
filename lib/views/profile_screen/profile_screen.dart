bddfae2723d62b3bd140bca65fd5740d0961dc81
Pranav <pranav.a.biju007@gmail.com>
2024-11-20 12:31:18 +0530

    profile_screen completed

--- lib/views/profile_screen/profile_screen.dart
+++ lib/views/profile_screen/profile_screen.dart
@@ -2,7 +2,26 @@ import 'package:flutter/material.dart';
 import 'package:indeed_ui/utils/color_contstants.dart';
 
 class ProfileScreen extends StatelessWidget {
-  const ProfileScreen({super.key});
+  ProfileScreen({super.key});
+
+  final List<Map<String, String>> jobImprovements = [
+    {
+      "HeadingText": "Qualifications",
+      "text": "Highlight your skills and experience."
+    },
+    {
+      "HeadingText": "Job Preferences",
+      "text": "Save specific details like minimum desired pay and schedule."
+    },
+    {
+      "HeadingText": "Hide Jobs with These Details",
+      "text": "Manage the qualifications or preferences and schedule."
+    },
+    {
+      "HeadingText": "Ready to Work",
+      "text": "Let employers know that you're available to start working as soon as possible."
+    },
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -27,166 +46,196 @@ class ProfileScreen extends StatelessWidget {
           ),
         ],
       ),
-      // backgroundColor: Colors.white,
-      body: Padding(
-        padding: const EdgeInsets.only(left: 15, right: 10),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            // Section
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  "PRANAV A BIJU",
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.only(left: 15, right: 10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "PRANAV A BIJU",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(right: 20),
-                  child: CircleAvatar(
-                    radius: 40,
-                    child: Text(
-                      "PA",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 16,
+                  Padding(
+                    padding: EdgeInsets.only(right: 20),
+                    child: CircleAvatar(
+                      radius: 40,
+                      child: Text(
+                        "PA",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
                       ),
+                      backgroundColor: Colors.black,
                     ),
-                    backgroundColor: Colors.black,
                   ),
-                ),
-              ],
-            ),
-            SizedBox(height: 30),
-            // Section
-            Column(
-              children: [
-                Row(
-                  children: [
-                    Icon(
-                      Icons.email,
-                      size: 30,
-                      color: textColor,
-                    ),
-                    SizedBox(width: 10),
-                    Text(
-                      "pranav.a.biju007@gmail.com",
-                      style: TextStyle(
-                        color: Colors.black,
-                      ),
-                    ),
-                  ],
-                ),
-                SizedBox(height: 5),
-                Row(
-                  children: [
-                    Icon(
-                      Icons.phone,
-                      size: 30,
-                      color: textColor,
-                    ),
-                    SizedBox(width: 10),
-                    Text(
-                      "858994678!",
-                      style: TextStyle(
-                        color: Colors.black,
-                      ),
-                    ),
-                    Spacer(),
-                    Icon(Icons.arrow_forward_ios, size: 20),
-                  ],
-                ),
-                SizedBox(height: 5),
-                Row(
-                  children: [
-                    Icon(
-                      Icons.location_on,
-                      size: 30,
-                      color: textColor,
-                    ),
-                    SizedBox(width: 10),
-                    Text(
-                      "Mvattupuzha H.O,Kochi,Kerala,682316,IN",
-                      style: TextStyle(
-                        color: Colors.black,
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-            SizedBox(height: 40),
-            Text(
-              "Resumes",
-              style: TextStyle(
-                color: Colors.black,
-                fontWeight: FontWeight.bold,
-                fontSize: 20,
+                ],
               ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            // Section
-            _bulidResumes(
-              icon: Icons.file_copy,
-              title: "indeed Resume",
-              updateDate: "Updated 27 Oct 2024",
-              isSerachable: true,
-              icon1: Icons.arrow_forward_ios,
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            _bulidResumes(
-              icon: Icons.picture_as_pdf,
-              title: "indeed Resume",
-              updateDate: "Added 28 Oct 2024",
-              isSerachable: false,
-              showSearchableRow: false,
-              icon1: Icons.more_vert_outlined,
-            ),
-            SizedBox(
-              height: 50,
-            ),
-            // Section
-            Text(
-              "improve your job matches", // data
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
+              SizedBox(height: 30),
+              Column(
+                children: [
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.email,
+                        size: 30,
+                        color: textColor,
+                      ),
+                      SizedBox(width: 10),
+                      Text(
+                        "pranav.a.biju007@gmail.com",
+                        style: TextStyle(
+                          color: Colors.black,
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 5),
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.phone,
+                        size: 30,
+                        color: textColor,
+                      ),
+                      SizedBox(width: 10),
+                      Text(
+                        "8589946781",
+                        style: TextStyle(
+                          color: Colors.black,
+                        ),
+                      ),
+                      Spacer(),
+                      Icon(Icons.arrow_forward_ios, size: 20),
+                    ],
+                  ),
+                  SizedBox(height: 5),
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.location_on,
+                        size: 30,
+                        color: textColor,
+                      ),
+                      SizedBox(width: 10),
+                      Text(
+                        "Mvattupuzha H.O, Kochi, Kerala, 682316, IN",
+                        style: TextStyle(
+                          color: Colors.black,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
               ),
-            ),
-            SizedBox(
-              height: 10,
-            ),
-            Divider(
-              color: Colors.black,
-              thickness: 0.5,
-            ),
-            Text("data")
-          ],
+              SizedBox(height: 40),
+              Text(
+                "Resumes",
+                style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                ),
+              ),
+              SizedBox(height: 10),
+              _buildResumes(
+                icon: Icons.file_copy,
+                title: "Indeed Resume",
+                updateDate: "Updated 27 Oct 2024",
+                isSearchable: true,
+                icon1: Icons.arrow_forward_ios,
+              ),
+              SizedBox(height: 10),
+              _buildResumes(
+                icon: Icons.picture_as_pdf,
+                title: "Uploaded Resume",
+                updateDate: "Added 28 Oct 2024",
+                isSearchable: false,
+                showSearchableRow: false,
+                icon1: Icons.more_vert_outlined,
+              ),
+              SizedBox(height: 50),
+              Text(
+                "Improve Your Job Matches",
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 10),
+              Divider(color: Colors.black, thickness: 0.5),
+              Column(
+                children: jobImprovements.map((job) {
+                  return _improveYourJobs(
+                    headingText: job["HeadingText"]!,
+                    text: job["text"]!,
+                  );
+                }).toList(),
+              ),
+              SizedBox(height: 50,),
+            ],
+          ),
         ),
       ),
     );
   }
 
-  Container _bulidResumes({
+  Widget _improveYourJobs({
+    required String headingText,
+    required String text,
+  }) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            headingText,
+            style: TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.bold,
+              fontSize: 16,
+            ),
+          ),
+          SizedBox(width: 4,),
+          Row(
+            children: [
+              Expanded(
+                child: Text(
+                  text,
+                  overflow: TextOverflow.fade,
+                  style: TextStyle(fontSize: 14),
+                ),
+              ),
+              Icon(Icons.arrow_forward_ios, size: 16),
+            ],
+          ),
+          Divider(color: Colors.black, thickness: 0.5),
+        ],
+      ),
+    );
+  }
+
+  Container _buildResumes({
     required IconData icon,
     required IconData icon1,
     required String title,
     required String updateDate,
-    required bool isSerachable,
+    required bool isSearchable,
     bool showSearchableRow = true,
   }) {
     return Container(
-      padding: EdgeInsets.only(left: 10, right: 10),
+      padding: EdgeInsets.symmetric(horizontal: 10),
       width: double.infinity,
       height: 100,
       decoration: BoxDecoration(
@@ -199,46 +248,49 @@ class ProfileScreen extends StatelessWidget {
       child: Row(
         children: [
           Icon(
-            // Icons.file_copy,
             icon,
             size: 60,
           ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                title,
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
+          SizedBox(width: 10),
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  title,
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-              ),
-              Text(
-                updateDate,
-                style: TextStyle(color: textColor),
-              ),
-              if (showSearchableRow)
-                Row(
-                  children: [
-                    Icon(
-                      Icons.remove_red_eye,
-                      color: textColor,
-                    ),
-                    SizedBox(width: 5),
-                    Text(
-                      "Searchable",
-                      style: TextStyle(
+                Text(
+                  updateDate,
+                  style: TextStyle(color: textColor),
+                ),
+                if (showSearchableRow)
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.remove_red_eye,
                         color: textColor,
-                        fontWeight: FontWeight.bold,
+                        size: 16,
                       ),
-                    ),
-                  ],
-                ),
-            ],
+                      SizedBox(width: 5),
+                      Text(
+                        "Searchable",
+                        style: TextStyle(
+                          color: textColor,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 12,
+                        ),
+                      ),
+                    ],
+                  ),
+              ],
+            ),
           ),
-          Spacer(),
           Icon(
             icon1,
             size: 20,
