a78123a6a5433977888a9f781786becfc152724f
Pranav <pranav.a.biju007@gmail.com>
2024-11-27 10:00:24 +0530

    setup

--- lib/views/detalis_screen/apply_screen/apply_screen.dart
+++ lib/views/detalis_screen/apply_screen/apply_screen.dart
@@ -39,30 +39,8 @@ class ApplyScreen extends StatelessWidget {
                   ),
                 ),
                 Text("Limenzy Technologies - Calicut, Kerala"),
-                GestureDetector(
-                  onTap: () {
-                    // Trigger the SnackBar
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      SnackBar(
-                        content: Text("Navigating to detailed information..."),
-                        // duration: Duration(seconds: 2),
-                        action: SnackBarAction(
-                          label: "CLOSE",
-                          onPressed: () {
-                            ScaffoldMessenger.of(context).hideCurrentSnackBar();
-                          },
-                        ),
-                      ),
-                    );
-                  },
-                  child: Text(
-                    "See more details",
-                    style: TextStyle(
-                      color: Colors.blue,
-                      decoration: TextDecoration.underline,
-                    ),
-                  ),
-                ),
+                _seeMoreDetails(context),
+                Text("Additional Information"),
               ],
             ),
           ),
@@ -70,4 +48,179 @@ class ApplyScreen extends StatelessWidget {
       ),
     );
   }
+
+GestureDetector _seeMoreDetails(BuildContext context) {
+  return GestureDetector(
+    onTap: () {
+      // Show a detailed modal bottom sheet
+      showModalBottomSheet(
+        context: context,
+        isScrollControlled: false,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(
+            top: Radius.circular(20),
+          ),
+        ),
+        builder: (context) {
+          return DraggableScrollableSheet(
+            initialChildSize: 0.8,
+            minChildSize: 0.5,
+            maxChildSize: 0.95,
+            expand: true,
+            builder: (context, scrollController) {
+              return SingleChildScrollView(
+                controller: scrollController,
+                padding: EdgeInsets.only(left: 10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "Full Job Description",
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        IconButton(
+                          icon: Icon(Icons.close),
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 10),
+                    Divider(
+                      thickness: 2,
+                      color: Colors.black,
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      "Internship Opportunity: Application Development",
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      "Are you passionate about software development? Do you want to gain real-world experience and grow your career? Join us!",
+                      style: TextStyle(fontSize: 14),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      "Position: Application Development Intern",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 14,
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      "What You'll Do:",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 14,
+                      ),
+                    ),
+                    SizedBox(height: 5),
+                    Text(
+                      "- Build and maintain Flutter & Dart applications.\n"
+                      "- Work on UI/UX to create dynamic, responsive designs.\n"
+                      "- Collaborate with cross-functional teams.\n"
+                      "- Debug and troubleshoot issues in live apps.",
+                      style: TextStyle(fontSize: 14),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      "Who You Are:",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 14,
+                      ),
+                    ),
+                    SizedBox(height: 5),
+                    Text(
+                      "- Knowledge of Flutter, Dart, and Firebase.\n"
+                      "- Familiarity with RESTful APIs and JSON data handling.\n"
+                      "- Strong problem-solving and analytical skills.\n"
+                      "- A passion for learning and adapting to new technologies.",
+                      style: TextStyle(fontSize: 14),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      "Why Join Us?",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 14,
+                      ),
+                    ),
+                    SizedBox(height: 5),
+                    Text(
+                      "- Hands-on experience in app development.\n"
+                      "- Flexible working environment.\n"
+                      "- Mentorship from experienced developers.\n"
+                      "- Opportunity to work on live projects and grow your skills.",
+                      style: TextStyle(fontSize: 14),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      "Location:",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 14,
+                      ),
+                    ),
+                    SizedBox(height: 5),
+                    Text(
+                      "Atreubrand Innovation Solutions, Pune, Maharashtra.",
+                      style: TextStyle(fontSize: 14),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      "Stipend:",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 14,
+                      ),
+                    ),
+                    SizedBox(height: 5),
+                    Text(
+                      "INR 5,000 - 20,000 based on performance.",
+                      style: TextStyle(fontSize: 14),
+                    ),
+                    SizedBox(height: 20),
+                    Text(
+                      "How to Apply:",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 14,
+                      ),
+                    ),
+                    SizedBox(height: 5),
+                    Text(
+                      "Send your CV and a brief cover letter to hr@atreubrand.com.",
+                      style: TextStyle(fontSize: 14),
+                    ),
+                    SizedBox(height: 20),
+                  ],
+                ),
+              );
+            },
+          );
+        },
+      );
+    },
+    child: Text(
+      "See more details",
+      style: TextStyle(
+        color: Colors.blue,
+        decoration: TextDecoration.underline,
+      ),
+    ),
+  );
+}
 }
